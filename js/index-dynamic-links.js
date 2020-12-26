6ad8ad20421a72a2f0020abf3c5f5bce83140e2e
CNOffers <codingninjasoffers@gmail.com>
2020-12-26 18:38:41 +0530

    referral link updated

--- js/index-dynamic-links.js
+++ js/index-dynamic-links.js
@@ -1,55 +1,53 @@
-var homepage=document.location.origin
-var referral_link="https://classroom.codingninjas.com/app/invite/TNMBH"
+var homepage = document.location.origin;
+// var referral_link="https://classroom.codingninjas.com/app/invite/TNMBH"
+var referral_link = 'https://www.codingninjas.com/?referralCode=TNMBH';
 
-
-function openReferalPage(){
-    window.open(referral_link, '_blank'); 
+function openReferalPage() {
+  window.open(referral_link, '_blank');
 }
 
-function openHomePage(){
-    window.open(homepage, '_self'); 
+function openHomePage() {
+  window.open(homepage, '_self');
 }
 
-function openInSameTab(link){
-    window.open(link, '_self'); 
+function openInSameTab(link) {
+  window.open(link, '_self');
 }
 
-function openInNewTab(link){
-    window.open(link, '_blank'); 
+function openInNewTab(link) {
+  window.open(link, '_blank');
 }
-
 
 // Referral Ka Sara
-$('#menu-cta-btn').add('#landing-cta-btn').add('#cta1-btn').add('#end-cta-join-btn')
-.on('click', openReferalPage);
-
+$('#menu-cta-btn')
+  .add('#landing-cta-btn')
+  .add('#cta1-btn')
+  .add('#end-cta-join-btn')
+  .on('click', openReferalPage);
 
 // Homepage
-$('#logo').add('')
-.on('click', openHomePage);
+$('#logo').add('').on('click', openHomePage);
 
 //Courses
-$('#tc-all-courses-btn')
-.on('click', (e)=>{
-    openInNewTab('https://www.codingninjas.com/courses')
+$('#tc-all-courses-btn').on('click', e => {
+  openInNewTab('https://www.codingninjas.com/courses');
 });
 
 // Free Trial
-$('#tc-free-trial-btn').add('#end-cta-try-free-btn')
-.on('click', (e)=>{
-    openInNewTab('https://www.codingninjas.com/start-learning')
-});
-
-
+$('#tc-free-trial-btn')
+  .add('#end-cta-try-free-btn')
+  .on('click', e => {
+    openInNewTab('https://www.codingninjas.com/start-learning');
+  });
 
 // Etc
 // How To Get Discounts
-$('#h2gd-check-ongoing-discounts-btn').on('click',(e)=>{
-    openInNewTab('https://www.codingninjas.com/')
+$('#h2gd-check-ongoing-discounts-btn').on('click', e => {
+  openInNewTab('https://www.codingninjas.com/');
 });
 
-$('#h2gd-my-referral-btn').on('click',openReferalPage);
+$('#h2gd-my-referral-btn').on('click', openReferalPage);
 
-$('#h2gd-check-fb-btn').on('click', (e)=>{
-    openInNewTab('https://www.facebook.com/codingninjas')
+$('#h2gd-check-fb-btn').on('click', e => {
+  openInNewTab('https://www.facebook.com/codingninjas');
 });
