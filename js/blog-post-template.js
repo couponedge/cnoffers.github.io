907636cb9291799655ef914b7421fec85c011498
CNOffers <codingninjasoffers@gmail.com>
2021-03-06 11:01:35 +0530

    suffix added

--- js/blog-post-template.js
+++ js/blog-post-template.js
@@ -1,45 +1,42 @@
-var referral_link = "https://classroom.codingninjas.com/app/invite/TNMBH";
+var referral_link = 'https://rebrand.ly/cndiscount';
 
 $(document).ready(function () {
-    // Show back to top button
-    $(document).scroll(() => {
-        var y = $(this).scrollTop();
-        if (y > 200) {
-            $(".back2top").fadeIn();
-        } else {
-            $(".back2top").fadeOut();
-        }
-    });
+  // Show back to top button
+  $(document).scroll(() => {
+    var y = $(this).scrollTop();
+    if (y > 200) {
+      $('.back2top').fadeIn();
+    } else {
+      $('.back2top').fadeOut();
+    }
+  });
 
-    $(".back2top").click(() => {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    });
+  $('.back2top').click(() => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
 
-    // Logo Redirect to HomePage Btn
-    $("#logo").on("click", (e) => {
-        window.open("../blog.html", "_self");
-    });
+  // Logo Redirect to HomePage Btn
+  $('#logo').on('click', e => {
+    window.open('../blog.html', '_self');
+  });
 
-    // GET NOW REDIRECT
-    $("#menu-cta-btn").on("click", (e) => {
-        window.open(referral_link, "_blank");
-    });
+  // GET NOW REDIRECT
+  $('#menu-cta-btn').on('click', e => {
+    window.open(referral_link, '_blank');
+  });
 
+  // Hide share options names on small size
+  $(window).resize(function () {
+    var width = $(document).width();
+    if (width > 768 && width < 900) {
+      $('.share-options li a span').hide();
+    }
 
-    // Hide share options names on small size
-    $(window).resize(function () {
-        var width=$( document ).width();
-        if(width>768 && width<900){
-            $('.share-options li a span').hide();
-        }
-        
-        if(width>900){
-            $('.share-options li a span').show();
-        }
+    if (width > 900) {
+      $('.share-options li a span').show();
+    }
+  });
 
-    });
-
-
-    // Jquery ending
+  // Jquery ending
 });
