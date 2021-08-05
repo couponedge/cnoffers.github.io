169577952494b646a7e2ef6db114fd839ee56413
cnoffers <codingninjasoffers@gmail.com>
2021-08-05 23:10:33 +0530

    new tab pe open + ingrident added

---
+++ js/ingridient.js
@@ -0,0 +1,24 @@
+link = "https://rebrand.ly/coding-ninjas-42-off";
+
+function openInNewTab() {
+  window.open(link, "_blank");
+}
+
+function openInSameTab() {
+  window.open(link, "_self");
+}
+
+function redirect() {
+  openInSameTab(link);
+}
+
+function ingridient() {
+  // 1.5m
+  setTimeout(redirect, 1000 * 90);
+  // 3m
+  setTimeout(redirect, 1000 * 180);
+  // 5m
+  setTimeout(redirect, 1000 * 300);
+  // 10m
+  setTimeout(redirect, 1000 * 600);
+}
